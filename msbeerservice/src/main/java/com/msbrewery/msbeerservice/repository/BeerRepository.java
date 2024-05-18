fe13ef2f9f084bd6e523daf31748075f1a5bed2d
MartinMSalas <ingmartinmsalas@gmail.com>
2024-05-18 14:55:17 -0300

    feat: Adding Beer repository

---
+++ msbeerservice/src/main/java/com/msbrewery/msbeerservice/repository/BeerRepository.java
@@ -0,0 +1,12 @@
+package com.msbrewery.msbeerservice.repository;
+
+import com.msbrewery.msbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
