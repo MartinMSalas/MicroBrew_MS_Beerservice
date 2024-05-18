4059cc09916f41b67e348b11db62eb278ef5a23c
MartinMSalas <ingmartinmsalas@gmail.com>
2024-05-18 10:53:49 -0300

    feat: Add files

---
+++ msbeerservice/src/main/java/com/msbrewery/msbeerservice/MsbeerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.msbrewery.msbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbeerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsbeerserviceApplication.class, args);
+	}
+
+}
---
+++ msbeerservice/src/test/java/com/msbrewery/msbeerservice/MsbeerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msbrewery.msbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsbeerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
